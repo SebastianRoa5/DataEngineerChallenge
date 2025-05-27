0710c0ecc5da5080d5dfc25a769db8561a128012
SebastianRoa5 <js.roa10@uniandes.edu.co>
2025-05-27 16:28:09 -0500

    expectations for raw data and some refactor

--- airflow/dags/ingestions/utils.py
+++ airflow/dags/ingestions/utils.py
@@ -1,22 +1,30 @@
 import pandas as pd
 from sodapy import Socrata
 from airflow.providers.postgres.hooks.postgres import PostgresHook
-from io import BytesIO
 import tempfile
 from datetime import datetime
 from airflow.utils.log.logging_mixin import LoggingMixin
+import great_expectations as gx
+from great_expectations.core.expectation_suite import (
+    ExpectationSuite
+)
+from airflow.exceptions import AirflowSkipException
+
+
 import os
+import json
+
 
 logger = LoggingMixin().log
 
-def get_fire_incidents(app_token, **context):
+def get_fire_incidents(app_token, key_csv_path, **context):
     client = Socrata(domain="data.sfgov.org", app_token=app_token)
     last_updated = context['ti'].xcom_pull(key='last_updated')
     logger.info(f"Last updated timestamp: {last_updated}")
     last_updated_format = last_updated.strftime('%Y-%m-%dT%H:%M:%S') if last_updated else None
     where_clause = f"data_loaded_at > '{last_updated_format}'" if last_updated else None
     logger.info(f"Where clause: {where_clause}")
-    results = client.get("wr8u-xric", content_type='csv', limit=1000000, where=where_clause)
+    results = client.get("wr8u-xric", content_type='csv', limit=100, where=where_clause)
     # Convert to pandas DataFrame
     results_df = pd.DataFrame.from_records(results)
     results_df.columns = results_df.iloc[0, :]
@@ -24,23 +32,29 @@ def get_fire_incidents(app_token, **context):
     results_df['created_at'] = datetime.utcnow()
     results_df['updated_at'] = datetime.utcnow()
     # Save DataFrame to a temporary CSV file for testing purposes, for more serious with AWS, should be S3
-    with tempfile.NamedTemporaryFile(delete=False, suffix=".csv") as tmpfile:
-        results_df.to_csv(tmpfile.name, index=False)
-        tmpfile_path = tmpfile.name
-    context['ti'].xcom_push(key='fire_incidents_csv_path', value=tmpfile_path)
-    logger.info(f"Saved fire incidents to temporary file: {tmpfile_path}")
+    if not results_df.empty:
+        with tempfile.NamedTemporaryFile(delete=False, suffix=".csv") as tmpfile:
+            results_df.to_csv(tmpfile.name, index=False)
+            tmpfile_path = tmpfile.name
+        context['ti'].xcom_push(key=key_csv_path, value=tmpfile_path)
+        logger.info(f"Saved fire incidents to temporary file: {tmpfile_path}")
+    else:
+        logger.info("No new fire incidents data to process.")
+        context['ti'].xcom_push(key=key_csv_path, value=None)
+        raise AirflowSkipException("No new data to insert or file not found.")
 
-def insert_data(**context):
+
+def insert_csv_raw_data(table,key_csv_path,**context):
     first_load = context['ti'].xcom_pull(key='first_load', task_ids='decide_load')
-    csv_path = context['ti'].xcom_pull(key='fire_incidents_csv_path')
+    csv_path = context['ti'].xcom_pull(key=key_csv_path)
     if not csv_path or not os.path.exists(csv_path):
         logger.info("No new data to insert or file not found.")
-        return
+        raise AirflowSkipException("No new data to insert or file not found.")
     pg_hook = PostgresHook(postgres_conn_id='postgres_default')
     if first_load:
         # Use COPY command for bulk loading
         pg_hook.copy_expert(
-            "COPY raw.fire_incidents FROM STDIN WITH CSV HEADER",
+            f"COPY raw.{table} FROM STDIN WITH CSV HEADER",
             csv_path
         )
     else:
@@ -54,7 +68,7 @@ def insert_data(**context):
         set_clause += ", updated_at = (now() AT TIME ZONE 'utc')"
         for _, row in df.iterrows():
             pg_hook.run(
-                f"INSERT INTO RAW.fire_incidents ({','.join(df.columns)}) VALUES ({','.join(['%s'] * len(df.columns))}) "
+                f"INSERT INTO RAW.{table} ({','.join(df.columns)}) VALUES ({','.join(['%s'] * len(df.columns))}) "
                 f"ON CONFLICT(id) DO UPDATE SET {set_clause}",
                 parameters=tuple(row)
             )
@@ -72,3 +86,16 @@ def decide_load(**context):
         context['ti'].xcom_push(key='last_updated', value=None)
         context['ti'].xcom_push(key='first_load', value=True)
         logger.info("First load, inserting by bulk.")
+
+
+def validate_csv_data(key_csv_path,expectation_suite_path,**context):
+    csv_path = context['ti'].xcom_pull(key=key_csv_path)
+    context = gx.get_context()
+    validator = context.sources.pandas_default.read_csv(csv_path)
+    # Define the Expectation to test:
+    with open(expectation_suite_path, 'r') as f:
+        expectation_suite = json.load(f)
+    expectation = ExpectationSuite(**expectation_suite)
+    # Test the Expectation:
+    validation_results = validator.validate(expectation)
+    logger.info(f"Validation results: {validation_results}")
